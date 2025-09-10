cf92a2a492faee47c75516e0d75a59e39ec4faea
isma <ismaelmed500@gmail.com>
2025-09-10 10:42:01 -0300

    messi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Agregamos CommonModule
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-tarjeta-credito',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule], // Necesario para que las directivas estructurales funcionen
+  templateUrl: './components/tarjeta-credito/tarjeta-credito.component.html',
+  styleUrl: './components/tarjeta-credito/tarjeta-credito.component.css'
 })
-export class AppComponent {
-  title = 'FEtarjetadecredito';
+export class TarjetaCreditoComponent implements OnInit {
+
+  // La lista de tarjetas ahora vive en este componente
+  listTarjetas: any[] = [
+    { titular: "Franco Mastantuono", numero: "1234567890123456", fechadeexpiracion: "9/12", cvv: "123" },
+    { titular: "Sebastian Driussi", numero: "4983844884474847", fechadeexpiracion: "9/12", cvv: "918" }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
---
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-credito',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarjeta-credito.component.html',
+  styleUrl: './tarjeta-credito.component.css'
+})
+export class TarjetaCreditoComponent {
+
+}
